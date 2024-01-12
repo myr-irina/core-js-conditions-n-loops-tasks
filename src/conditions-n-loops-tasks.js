22379c7ba0f89e5ac3479960e57281579019a179
Irina <myr-irina2@yandex.ru>
2024-01-12 17:47:33 +0300

    fix: add solution to isIsoscelesTriangle task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -97,9 +97,14 @@ console.log({ x: 1, y: 1 }, { x: 2, y: 8 });
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a === 0 || b === 0 || c === 0) return false;
+  return (
+    (a === b && a + b > c) || (b === c && b + c > a) || (a === c && a + c > b)
+  );
 }
+
+console.log(isIsoscelesTriangle(2, 2, 5));
 
 /**
  * Converts a number to Roman numerals. The number will be between 1 and 39.
